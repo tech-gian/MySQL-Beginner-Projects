ca20bb2a96582c8c8f119947cf2a181cce7c1b6f
tech-gian <sdi1900059@di.uoa.gr>
2021-06-26 12:31:35 +0300

    Add, settings for user

---
+++ Vaccinations/settings.py
@@ -0,0 +1,10 @@
+# MySQL Settings
+mysql_host =  '127.0.0.1' 
+mysql_port = 3306  # must be integer (this is wrong:'3306')
+mysql_user = ''   #must replace with your own settings
+mysql_passwd = '' #must replace with your own settings 
+mysql_schema = 'hospital'
+
+# Webserver Settings
+# IMPORTANT: The port must be available.
+web_port = 8080  # must be integer (this is wrong:'8080')
